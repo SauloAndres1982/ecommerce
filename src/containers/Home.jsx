41452076441a8b09ce67c9563e52dc1cdc516378
SauloAndres1982 <saulo.carmona@soyrosarista.edu.co>
2024-07-12 20:24:48 -0500

    first commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from "../hocs/Layout";
+
+const Home = () =>{
+    return(
+        <Layout>
+            <div className="text-red-500">HOME</div>
+        </Layout>
+    )    
+}
+
+export default Home
